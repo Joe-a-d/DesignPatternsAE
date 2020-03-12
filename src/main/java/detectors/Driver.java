9912b950b03bec860ebb20536ac5fd783c6cd228
Joe-a-d <2334590D@student.gla.ac.uk>
2020-03-12 18:39:29 +0000

    file handler

--- src/main/java/detectors/Driver.java
+++ src/main/java/detectors/Driver.java
@@ -1,8 +1,30 @@
 package detectors;
 
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.StaticJavaParser;
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * @author Domingues, Joao #2334590D
  */
 
 public class Driver {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        final String cd = System.getProperty("user.dir");
+        final JFileChooser fc = new JFileChooser();
+        fc.setCurrentDirectory(new File (cd));
+        int returnVal = fc.showOpenDialog(null); // set to null b/ no event binding
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            CompilationUnit cu = StaticJavaParser.parse(file);
+            System.out.println(cu);
+
+        }
+    }
 }
+
